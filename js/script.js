21b1d71d0ae4dba977a5bb4b79a0495f2db4a013
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-05-10 17:26:25 +0200

    realizzazione e inserimento in HTML di un elemento in js che rispetti la condizione del ciclo for e che abbia delle caratteristiche proprie

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-
+const ulElement = document.querySelector('ul');
 
 for (let i = 0; i <= 100; i++){
 
     if (i % 3 === 0 && i % 5 === 0){
+        const liElement = document.createElement('li');
+        liElement.classList.add('fizz-buzz');
+        liElement.append('Fizzbuzz');
+        ulElement.appendChild(liElement);
         console.log('FizzBuzz')
     } else if (i % 3 === 0){
+        const liElement = document.createElement('li');
+        liElement.classList.add('fizz');
+        liElement.append('Fizz');
+        ulElement.appendChild(liElement);
         console.log('Fizz')
     } else if (i % 5 === 0){
+        const liElement = document.createElement('li');
+        liElement.classList.add('buzz');
+        liElement.append('Buzz');
+        ulElement.appendChild(liElement);
         console.log('Buzz')
     } else {
+        const liElement = document.createElement('li');
+        liElement.classList.add('number');
+        liElement.append(i);
+        ulElement.appendChild(liElement);
         console.log(i)
     }
 }
